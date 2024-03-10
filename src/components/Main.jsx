a91d5a8528f754c304ce9e0876f5348ee75811a6
Gouranshvaishnavji <gouransh.v@kalvium.community>
2024-03-10 23:15:19 +0530

    first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,37 @@
-import React, { useEffect } from 'react'
-import { useDispatch,useSelector } from 'react-redux'
-import {listAction, listSelector} from '../redux/reducer'
-import { fetchUser } from '../redux/reducer';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listAction, listSelector, toggleDataAction } from '../redux/reducer'; // Import toggleDataAction
 
 const Main = () => {
+  const dispatch = useDispatch();
+  const list = useSelector(listSelector);
+  const [dataVisible, setDataVisible] = useState(true); // State for data visibility
 
-    const dispatch = useDispatch();
-
-    const list = useSelector(listSelector);
-
-    useEffect(() => {
+  useEffect(() => {
     dispatch(fetchUser());
-    }, []);
+  }, []);
+
+  const toggleData = () => {
+    setDataVisible(!dataVisible); // Toggle data visibility state
+    dispatch(toggleDataAction(dataVisible)); // Dispatch action to update data visibility in redux (if necessary)
+  };
 
   return (
-<div className="listContainer">
-  {list.map((item, index) => {
-    return (
-      <div className="listItem" key={index}>
-        <p className="name">{item.name}</p>
-        <p className="email">{item.email}</p>
-        <hr />
-      </div>
-    );
-  })}
-</div>
-  )
-}
+    <div className="listContainer">
+      <button onClick={toggleData}>{dataVisible ? 'Hide Data' : 'Show Data'}</button>
+      {dataVisible && ( // Conditionally render data if visible
+        <div>
+          {list.map((item, index) => (
+            <div className="listItem" key={index}>
+              <p className="name">{item.name}</p>
+              <p className="email">{item.email}</p>
+              <hr />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Main;
